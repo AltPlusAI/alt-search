cf3c23b27ae70f80c64bc78787569647b9528523
Divij <divijvignesh12@gmail.com>
2023-09-20 16:54:00 +0530

    Added authentication APIs

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("searchResults", views.get_GoogleResults, name="searchresults"),
     path("url",views.get_UrlData, name="getsummary"),
-    path('qnaResults', views.get_QnaResults,name="qna")
+    path('qnaResults', views.get_QnaResults,name="qna"),
 ]
