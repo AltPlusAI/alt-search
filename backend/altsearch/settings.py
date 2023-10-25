4c12aea6875e7df13076a977206e11ff16c3ca74
Divij <divijvignesh12@gmail.com>
2023-10-25 13:51:54 +0530

    added host for pythonanywhere

--- backend/altsearch/settings.py
+++ backend/altsearch/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--e1hgrmknbmi^&m=vu!@&tzbm4k4o%w33o773ajaabraw#*x*j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1',"https://altplusai.github.io"]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1',"https://altplusai.github.io","https://altplusai.pythonanywhere.com"]
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
