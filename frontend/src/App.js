e68770f431ca3b48b5b830ad47c2b1c7855cb625
Divij <divijvignesh12@gmail.com>
2023-10-25 14:05:46 +0530

    Update: Added baseurl for pythonanywhere

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 axios.defaults.withCredentials = true;
 
 const client = axios.create({
-  baseURL: "http://127.0.0.1:8000"
+  baseURL: "http://altplusai.pythonanywhere.com"
 });
 
 function App() {
--- frontend/src/components/Query/Query.js
+++ frontend/src/components/Query/Query.js
@@ -105,7 +105,7 @@ const Query = () => {
         addMessage(newMessage);
     };
     const client = axios.create({
-        baseURL: "http://127.0.0.1:8000"
+        baseURL: "http://altplusai.pythonanywhere.com"
     });
     // Adds links to a variable and sets the value of links
     function selectLink() {
