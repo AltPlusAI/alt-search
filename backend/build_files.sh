3e8a4b6c673000bea666e2c3d313458b314e8a6f
Divij Vignesh <72255233+DivijVignesh@users.noreply.github.com>
2023-10-26 18:31:29 +0530

    Update build_files.sh

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,3 +1,4 @@
- echo "BUILD START"
- python3.9 -m pip install -r requirements.txt
- echo "BUILD END"
+# build_files.sh
+
+pip install -r requirements.txt
+python3.9 manage.py collectstatic --no-input --clear
